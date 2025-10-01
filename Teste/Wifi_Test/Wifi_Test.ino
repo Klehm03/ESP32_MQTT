697d9e046508a2ce5cf83cad4dea3d968a91375b
ISAQUE KLEHM <isaque_klehm@estudante.sesisenai.org.br>
2025-10-01 15:25:53 -0300

    Upgrade do código

--- Teste/Wifi_Test/Wifi_Test.ino
+++ Teste/Wifi_Test/Wifi_Test.ino
@@ -2,10 +2,21 @@
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("Iniciando Scan de Rede Wi-Fi");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int number = WiFi.scanNetworks();
+  delay(500);
+  if (number == -1){
+    Serial.println("ERRO!");
+  }
+  else{
+    for(int net = 0; net < number; net++){
+      Serial.printf("%d - %s | %d db\n", net ,WiFi.SSID(net), WiFi.RSSI(net));
+    }
+    //Serial.printf("Número de redes encontradas: %d\n",number);
+  }
 }
