77b5d673af515906f9cc01d4b271853310d2d19d
ISAQUE KLEHM <isaque_klehm@estudante.sesisenai.org.br>
2025-10-01 16:15:22 -0300

    Wifi Test

--- Teste/Wifi_Test/Wifi_Test.ino
+++ Teste/Wifi_Test/Wifi_Test.ino
@@ -1,20 +1,29 @@
 #include <WiFi.h>
+const String SSID = "iPhone";
+const String PSW = "iot_sul_123";
 
 void scanLocalNetWork();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Iniciando Scan de Rede Wi-Fi");
   scanLocalNetWork();
+  Serial.println("Iniciando conexão com rede WiFi");
+  WiFi.begin(SSID, PSW);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado");
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   
 }
 
 void scanLocalNetWork(){
-  // put your main code here, to run repeatedly:
+  Serial.println("Iniciando Scan de Rede Wi-Fi");
   int number = WiFi.scanNetworks();
   delay(500);
   if (number == -1){
