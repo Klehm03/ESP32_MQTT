af138adc327743ce5f282c534e1c72929720cade
ISAQUE KLEHM <isaque_klehm@estudante.sesisenai.org.br>
2025-10-01 17:04:10 -0300

    Conectando e reconectando WiFi

--- Teste/Wifi_Test/Wifi_Test.ino
+++ Teste/Wifi_Test/Wifi_Test.ino
@@ -1,38 +1,58 @@
-#include <WiFi.h>
-const String SSID = "iPhone";
-const String PSW = "iot_sul_123";
+#include <WiFi.h> 
+const String SSID = "A55 de Isaque";
+const String PSWD = "12345678";
 
-void scanLocalNetWork();
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  scanLocalNetWork();
-  Serial.println("Iniciando conexão com rede WiFi");
-  WiFi.begin(SSID, PSW);
+void scanLocalNetworks();
+
+void conexaoWifi(){
+  WiFi.begin(SSID,PSWD);
+  Serial.println("Iniciando conexão com rede Wi-Fi");
+  Serial.print("Conectando");
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado");
+  Serial.println("\nConectado!");
+}
+
+void reconexaoWifi(){
+  
+  Serial.println("Iniciando reconexão com rede Wi-Fi");
+  Serial.print("Reconectando");
+  while(WiFi.status() != WL_CONNECTED){
+    WiFi.begin(SSID,PSWD);
+    Serial.print(".");
+    delay(2000);
+  }
+  Serial.println("\nReconectado!");
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  scanLocalNetworks();
+  conexaoWifi();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  if(WiFi.status() != WL_CONNECTED){
+    reconexaoWifi();
+  }
 }
 
-void scanLocalNetWork(){
-  Serial.println("Iniciando Scan de Rede Wi-Fi");
+void scanLocalNetworks(){
+  Serial.println("Iniciando o Scan de redes Wi-Fi");
   int number = WiFi.scanNetworks();
-  delay(500);
-  if (number == -1){
-    Serial.println("ERRO!");
-  }
-  else{
-    for(int net = 0; net < number; net++){
-      Serial.printf("%d - %s | %d db\n", net ,WiFi.SSID(net), WiFi.RSSI(net));
+    delay(500);
+    if(number == -1){
+      Serial.println("ERRO!, problema em encontrar alguma rede");
     }
-    //Serial.printf("Número de redes encontradas: %d\n",number);
-  }
+    else{
+      for(int net = 0; net < net; net++){
+        Serial.printf("%d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+      }
+      Serial.printf("Número de redes encontradas: %d\n", number);
+
+    }
 }
