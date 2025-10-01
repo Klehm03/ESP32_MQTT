2947f2d0bd9c03d8775c971a5b8863feda2ef9c0
ISAQUE KLEHM <isaque_klehm@estudante.sesisenai.org.br>
2025-10-01 17:28:39 -0300

    ESP32 com conexão ao broker mosquitto

---
+++ Wifi_Test_com_broker/Wifi_Test_com_broker.ino
@@ -0,0 +1,82 @@
+#include <WiFi.h> 
+#include <PubSubClient.h>
+
+const String SSID = "A55 de Isaque";
+const String PSWD = "12345678";
+
+const String brokerUrl = "test.mosquitto.org";
+const int port = 1883;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void connectToWifi(){
+connectToWifi();
+mqttClient.setServer(brokerUrl.c_str(), port);
+String userId = "ESP-BANANINHA";
+mqttClient.connect(userId.c_str());
+}
+
+void scanLocalNetworks();
+
+void conexaoWifi(){
+  WiFi.begin(SSID,PSWD);
+  Serial.println("Iniciando conexão com rede Wi-Fi");
+  Serial.print("Conectando");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado!");
+}
+
+void reconexaoWifi(){
+  
+  Serial.println("Iniciando reconexão com rede Wi-Fi");
+  Serial.print("Reconectando");
+  while(WiFi.status() != WL_CONNECTED){
+    WiFi.begin(SSID,PSWD);
+    Serial.print(".");
+    delay(2000);
+  }
+  Serial.println("\nReconectado!");
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  scanLocalNetworks();
+  conexaoWifi();
+  Serial.println("Conectando ao broker");
+  mqttClient.setServer(brokerUrl.c_str(),port);
+  String userId = "ESP-BANANINHA";
+  mqttClient.connect(userId.c_str());
+  while(!mqttClient.connected()){
+    Serial.println("Erro de conexão");
+    delay(500);
+  }
+  Serial.println("mqtt Connectado com sucesso!");
+}
+
+void loop() {
+  if(WiFi.status() != WL_CONNECTED){
+    reconexaoWifi();
+  }
+  mqttClient.loop();
+}
+
+void scanLocalNetworks(){
+  Serial.println("Iniciando o Scan de redes Wi-Fi");
+  int number = WiFi.scanNetworks();
+    delay(500);
+    if(number == -1){
+      Serial.println("ERRO!, problema em encontrar alguma rede");
+    }
+    else{
+      for(int net = 0; net < net; net++){
+        Serial.printf("%d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+      }
+      Serial.printf("Número de redes encontradas: %d\n", number);
+
+    }
+}
